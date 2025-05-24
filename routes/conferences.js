3cc18c347880048fc0c2318f270744b7af7a1ead
Manish Dhubkarya <147823856+Manish-Dhubkarya@users.noreply.github.com>
2025-05-24 18:02:16 +0530

    conference submit data

--- routes/conferences.js
+++ routes/conferences.js
@@ -1,10 +1,81 @@
 var express = require('express');
 var router = express.Router();
+var pool = require("./pool")
+// var upload = require("./multer");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('conferences', { title: 'Conference' });
+router.post('/submit_conference', function (req, res, next) {
+    console.log("Bzzzzzzzzzzzz",req)
+    try {
+        pool.query("insert into conferences (publisher, conferenceName, area, subject, lastDOfSub, registrationCharges, links) values(?,?,?,?,?,?,?)", [req.body.publisher, req.body.conferenceName, req.body.area, req.body.subject, req.body.lastDOfSub, req.body.registrationCharges, req.body.links], function (error, result) {            
+            if (error) {
+                console.log("ERRRRRRRRR",error)
+               return res.status(400).json({ status: false, message: "DataBase Error, Plz Contact DataBase Admin" })
+            }
+            else {
+               return res.status(200).json({ status: true, message: "Conference Submitted Successfully!" })
+            }
+        })
+    }
+    catch (e) {
+      return  res.status(500).json({ status: false, message: "Server Error...!" })
+
+    }
+});
+router.post('/update_conference', function (req, res, next) {
+    console.log(req.body)
+    console.log(req.file)
+    try {
+        pool.query("update category set categoryname=? where categoryid=?", [req.body.categoryname, req.body.categoryid], function (error, result) {
+            if (error) {
+                console.log(error)
+               return res.status(500).json({ status: false, message: "DataBase Error, Plz Contact DataBase Admin" })
+            }
+            else {
+               return res.status(200).json({ status: true, message: "Category Updated Successfully!" })
+            }
+        })
+    }
+    catch (e) {
+      return  res.status(500).json({ status: false, message: "Server Error...!" })
+
+    }
 });
 
+router.post('/delete_conference', function (req, res, next) {
+    console.log(req.body)
+    console.log(req.file)
+    try {
+        pool.query("delete from category where conferenceId=?", [req.body.conferenceId], function (error, result) {
+            if (error) {
+                console.log(error)
+               return res.status(500).json({ status: false, message: "DataBase Error, Plz Contact DataBase Admin" })
+            }
+            else {
+               return res.status(200).json({ status: true, message: "Category Deleted Successfully!" })
+            }
+        })
+    }
+    catch (e) {
+      return  res.status(500).json({ status: false, message: "Server Error...!" })
 
+    }
+});
+router.get("/display_all_category", function(req, res, next){
+    
+    try {
+        pool.query("select * from category", function (error, result) {
+            if (error) {
+                console.log(error)
+               return res.status(500).json({status: false, message: "DataBase Error, Plz Contact DataBase Admin"})
+            }
+            else {
+               return res.status(200).json({data:result, status: true, message: "Success!" })
+            }
+            
+        })
+    }
+    catch (e) {
+      return  res.status(500).json({ status: false, message: "Server Error...!" })
+    }
+})
 module.exports = router;
